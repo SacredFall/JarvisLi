70519f6fca5448d396b9fd8612236fa51e5c7cf0
Eric Chen <ericc404@nycstudents.net>
2024-10-24 13:58:55 -0400

    fdbfd

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function factorial(x) {
+/* function factorial(x) {
   let answer = 1;
   for (let i = 1; i <= x; i++) {
     answer *= i;
@@ -7,16 +7,33 @@ function factorial(x) {
 }
 
 console.log(factorial(0.5));
-
+ */
 //Occupy Parking
-function parking(n, a, b) {
-  let sum = 0;
-  for (let i = 1; i <= n; i++) {
-    if ((a[i] === b[i]) === "C") {
-      sum += 1;
+/* function parking(n, a, b) {
+  let number = 0;
+  for (let i = 0; i < n; i++) {
+    if (a[i] == "C" && b[i] == "C") {
+      number += 1;
     }
   }
-  return sum;
+  return number;
 }
 
-console.log(parking(5, "C.C..", "C.C.."));
+console.log(parking(5, "CCCC.", "C.CC.")); */
+
+/* Psuedocode decimal to binary */
+
+function decimal(n) {
+  let n1 = String(n);
+  let summ = 0;
+  for (let i = 0; i < n1.length; i++) {
+    if (n1[i] == 1) {
+      summ += 2 ** (n1.length - 1 - i);
+    }
+  }
+  return summ;
+}
+
+console.log(decimal(1010));
+
+console.log(decimal(34234));
--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-for in range len()
+
+def decimal(n):
+    return int(str(n),2)
+
+
+
+print(decimal(1010))
+
+#----------
+
+def binary(n):
+    return bin(n)[2:]
+
+print(binary(10))
